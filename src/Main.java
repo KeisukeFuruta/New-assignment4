2cfaaa37da7cfe764166189cf083e782d35d63a4
KeisukeFuruta <kcomethunter@gmail.com>
2024-05-18 10:45:49 +0900

    mynameをリファクタリング。myNameへ変更

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
         //文字列結合と数値計算結果の結合をして
         //自分の名前　+　自分の年齢　+　"歳です。"が出力されるようにする。
 
-        String myname = "古田慧介";
+        String myName = "古田慧介";
         int age = 29;
 
-        System.out.println(myname + "は" + age + "歳です。");
+        System.out.println(myName + "は" + age + "歳です。");
 
         List<String> names = List.of("yamada","tanaka","yasui","suzuki","iida");
         List<String> result = names.stream().filter(name ->name.contains("a")).toList();
