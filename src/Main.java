99054358287fd85fc61cde637198b128fda3470e
KeisukeFuruta <kcomethunter@gmail.com>
2024-05-18 09:39:12 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //課題4
+        //文字列結合と数値計算結果の結合をして
+        //自分の名前　+　自分の年齢　+　"歳です。"が出力されるようにする。
+
+        String name = "古田慧介";
+        int age = 29;
+
+        System.out.println(name + "は" + age + "歳です。");
+        }
+    }
